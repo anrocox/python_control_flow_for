773d0df162541b22539764f3dcee0bf1c2b621e7
Andres Rodriguez <anrocox@gmail.com>
2014-08-01 09:04:29 -0500

    In Python, how to define a for loop inline?

---
+++ src/control_flow_for.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 23, 2014
+
+@author: anroco
+
+In Python, how to define a for loop inline?
+
+En Python, ¿Cómo se define un ciclo for en una linea de código?
+'''
+
+#the for loop iterate over the items of any sequence such as a list or a string
+
+#create a string
+s = 'Hello'
+
+#iterates over the items of any sequence 's' in the order in which they appear
+#in the sequence.
+for i in s:
+
+    #print i, where in each iteration is a letter of the word 'Hello'
+    print(i)
